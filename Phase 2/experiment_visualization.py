12282ec598ee3d076baf2e7e78364c4cb2a77366
Parkersep <parkerlsepulvado@gmail.com>
2024-10-08 22:36:00 -0500

    Parker branch (#12)
    * dev: finsihed func 4
    
    * dev: updated visualiztion
    
    * dev: update files
    
    * i hope this works

--- Phase 2/experiment_visualization.py
+++ Phase 2/experiment_visualization.py
@@ -5,45 +5,27 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-rover = {
-  'wheel_assembly': {
-      'wheel': {'radius': 0.30, 'mass': 1.0
-                },
-      
-      'motor': {'torque_stall': 170, 'torque_noload': 0, 'speed_noload': 3.80, 'mass': 5.0, 
-                'effcy_tau' : np.array([0, 10, 20, 40, 70, 165]) ,
-     'effcy' : np.array([0, .55,.75,.71,.50,.05])
-                },
-      
-      'speed_reducer': {'type': 'reverted', 'diam_pinion': 0.04, 'diam_gear': 0.07, 'mass': 1.5}
-  },
-  'chassis': {'mass': 659
-              },
-  
-  'science_payload': {'mass': 75
-                      },
-  
-  'power_subsys': {'mass': 90
-                   }
-}
-
-planet = {'g': 3.72}
+experiment, end_event = experiment1()
+alpha_dist = experiment['alpha_dist']
+alpha_deg = experiment['alpha_deg']
 
 
-experiment , end_event = experiment1()
+
+alpha_fun = interp1d(alpha_dist, alpha_deg, kind = 'cubic', fill_value='extrapolate') #fit the cubic spline
 
 
-effcy_tau = rover['wheel_assembly']['motor']['effcy_tau']   
-effcy = rover['wheel_assembly']['motor']['effcy']
+x = np.linspace(alpha_dist[0], alpha_dist[-1], 100)
 
-effcy_fun = interp1d(effcy_tau, effcy, kind = 'cubic', fill_value='extrapolate') #fit the cubic spline
 
-x = np.linspace(effcy_tau[0], effcy_tau[-1], 100)
+y= []
+for i in range(len(x)):
+    y.append(alpha_fun(x[i]))
 
-plt.plot(x, effcy_fun(x),label = 'Cubic Spline')
-plt.plot(effcy_tau, effcy, '*', label = 'Data Points', )
-plt.xlabel('Torque (N-m)')
-plt.ylabel('Efficiency')
-plt.title('Efficiency vs Torque')
+
+plt.plot(x, y,label = 'Cubic Spline')
+plt.plot(alpha_dist, alpha_deg, '*', label = 'Data Points', )
+plt.xlabel('Distance (m)')
+plt.ylabel('Alpha (degrees)')
+plt.title('Alpha vs Distance')
 plt.legend()
 plt.show()
