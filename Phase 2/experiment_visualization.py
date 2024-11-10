2de2a884cde540d8d8c1ae258f44d0274ec2959f
Parkersep <parkerlsepulvado@gmail.com>
2024-11-10 12:20:27 -0600

    dev: addded update code

--- Phase 2/experiment_visualization.py
+++ Phase 2/experiment_visualization.py
@@ -4,24 +4,24 @@ from scipy.interpolate import interp1d
 import numpy as np
 from matplotlib import pyplot as plt
 
-
+# define experiment and end_event along with the angle at different distances
 experiment, end_event = experiment1()
 alpha_dist = experiment['alpha_dist']
 alpha_deg = experiment['alpha_deg']
 
 
-
+#create a interpolation function to interpolate the data
 alpha_fun = interp1d(alpha_dist, alpha_deg, kind = 'cubic', fill_value='extrapolate') #fit the cubic spline
 
-
+# create a continuous range of distance values for plotting
 x = np.linspace(alpha_dist[0], alpha_dist[-1], 100)
 
-
+#get the corresponding alpha values
 y= []
 for i in range(len(x)):
     y.append(alpha_fun(x[i]))
 
-
+#plot alpha vs distance as a continous function and data points
 plt.plot(x, y,label = 'Cubic Spline')
 plt.plot(alpha_dist, alpha_deg, '*', label = 'Data Points', )
 plt.xlabel('Distance (m)')
